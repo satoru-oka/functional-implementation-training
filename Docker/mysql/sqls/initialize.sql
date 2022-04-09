2d3752b5149e6ede30361c2564906da5835fff03
satoru <40satoruoka10@gmail.com>
2022-04-09 21:53:03 +0900

    first commit

---
+++ Docker/mysql/sqls/initialize.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE qa_app;
+use qa_app;
+
+CREATE TABLE `answers` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `question_id` int(10) unsigned NOT NULL,
+    `user_id` int(10) unsigned NOT NULL,
+    `body` varchar(256) NOT NULL,
+    `created` datetime NOT NULL,
+    `modified` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `quesions` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `question_id` int(10) unsigned NOT NULL,
+    `user_id` int(10) unsigned NOT NULL,
+    `body` varchar(256) NOT NULL,
+    `created` datetime NOT NULL,
+    `modified` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `users` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `question_id` int(10) unsigned NOT NULL,
+    `user_id` int(10) unsigned NOT NULL,
+    `body` varchar(256) NOT NULL,
+    `created` datetime NOT NULL,
+    `modified` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
